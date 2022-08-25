4469d35b4979895cccc309445eba0535d8dc14de
harlem512 <freeharlem@gmail.com>
2022-08-25 15:58:09 -0400

    disable

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ import requests
 USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.107 Safari/537.36'
 URL_REGEX = r'(https?://www\.youtube\.com/watch\?v=[^\s]+)'
 invisible = True
-enabled = True
+enabled = False
 
 
 def time_print(s: str):
