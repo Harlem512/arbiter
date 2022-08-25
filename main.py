0338c380e3601e6dfc7e76db30c5e98f3cf85f76
harlem512 <freeharlem@gmail.com>
2022-08-25 15:57:43 -0400

    updates

--- main.py
+++ main.py
@@ -11,11 +11,17 @@ import requests
 
 USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.107 Safari/537.36'
 URL_REGEX = r'(https?://www\.youtube\.com/watch\?v=[^\s]+)'
-silent = True
+invisible = True
+enabled = True
 
 
 def time_print(s: str):
     print(f'{datetime.now()} | {s}')
+
+
+def name(member):
+    return member.nick if member.nick else member.name
+
 
 with open(os.path.join(sys.path[0], 'data.json'), 'r') as read:
     data = json.load(read)
@@ -34,25 +40,49 @@ join_set = set()
 
 class Arbiter(discord.Client):
     async def on_ready(self):
+        global invisible
+
         time_print(f'Logged on as {self.user}')
 
+        if enabled:
+            time_print('Enabled')
+
+        if invisible:
+            time_print('Going incognito')
+            await self.change_presence(status=discord.Status.offline)
+
     async def on_message(self, message):
-        global silent
+        global invisible
+        global enabled
 
-        if message.content == '!!!toggle':
-            silent = not silent
-            return
+        match message.content:
+            case 'hey arbiter':
+                return await message.channel.send('hello')
+            case '!!!invis':
+                invisible = not invisible
+                await self.change_presence(
+                    status=discord.Status.offline
+                    if invisible
+                    else discord.Status.online,
+                    activity=discord.Activity(
+                        type=discord.ActivityType.watching,
+                        name='for villains',
+                    ),
+                )
+                time_print(f'Invisible: {invisible} by {name(message.author)}')
+                return
+            case '!!!enable':
+                enabled = not enabled
+                time_print(f'Enable: {enabled} by {name(message.author)}')
+                return
 
-        if silent:
-            return
-
-        if not ALLOW_MITCHPOSTING:
-            return
-
-        if message.author == self.user:
+        if not enabled or not ALLOW_MITCHPOSTING or message.author == self.user:
             return
 
         urls = re.findall(URL_REGEX, message.content)
+
+        if len(urls) > 0:
+            return
 
         mitchpost = False
 
@@ -76,15 +106,14 @@ class Arbiter(discord.Client):
             await message.channel.send(
                 f'{message.author.mention} Mitchposting is not allowed'
             )
-            time_print(f'Deleted mitchpost `{message.content}` by {message.author.mention}')
+            time_print(
+                f'Deleted mitchpost `{message.content}` by {name(message.author)}'
+            )
 
     async def on_voice_state_update(self, member, before, after):
-        global silent
+        global invisible
 
-        if silent:
-            return
-
-        if after.channel == None:
+        if invisible or after.channel == None:
             return
 
         if after.self_mute and after.self_deaf:
@@ -101,7 +130,7 @@ class Arbiter(discord.Client):
                         join_set.remove(member.id)
 
             await member.edit(voice_channel=None, reason='deaf-mute is not allowed')
-            time_print(f'Kicked user {member.mention}')
+            time_print(f'Kicked user {name(member)}')
 
         async with join_lock:
             if member.id in join_set:
@@ -112,10 +141,10 @@ intents = discord.Intents.default()
 intents.message_content = True
 client = Arbiter(
     intents=intents,
-    activity=discord.Activity(type=discord.ActivityType.watching, name='for villains'),
+    activity=discord.Activity(
+        type=discord.ActivityType.watching,
+        name='for villains',
+    ),
 )
-if silent:
-    time_print('Going incognito...')
-    client.change_presence(status=discord.Status.offline)
 
 client.run(data['token'])
